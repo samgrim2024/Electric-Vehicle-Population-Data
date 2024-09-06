e0e25a3c7e32696789699ff4ae4841996bd4a315
Samanta <samgrim2024@gmail.com>
2024-09-06 20:22:55 +0100

    add connection.py

---
+++ connection.py
@@ -0,0 +1,13 @@
+import os
+import sqlalchemy import create_engine
+
+DATABASE_TYPE = 'postgresql'
+DBAPI = 'pg8000'
+HOST = 'localhost'
+USER = os.getenv('DB_USER', 'postgres')
+PASSWORD = os.getenv('DB_PASSWORD', '')
+DATABASE = 'ev_database'
+PORT = 5432
+
+engine = create_engine(f'{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
+
